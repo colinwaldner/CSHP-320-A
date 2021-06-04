e7e775703a7cd693d3ba2e11c6d87cd7d8d54a86
colinwaldner-homepc <colin.waldner@outlook.com>
2021-06-03 22:44:28 -0500

    New Project - Session 8 MVC

---
+++ Session8MVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Session8MVC.Models;
+
+namespace Session8MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View(new GuestResponse());
+        }
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View(guestResponse);
+            }
+            
+        }
+    }
+}
---
+++ Session8MVC/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Session8MVC.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please Enter Your Name.")]
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
