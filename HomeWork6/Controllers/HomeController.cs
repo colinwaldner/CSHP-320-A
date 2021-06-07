4f60ef6c8f75a0db65db26d066e454ade74785eb
colinwaldner-homepc <colin.waldner@outlook.com>
2021-06-07 11:43:44 -0500

    Completed Home Work 6

---
+++ HomeWork6/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeWork6.Models;
+
+namespace HomeWork6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new BirthdayCard());
+        }
+        [HttpPost]
+        public IActionResult Index(BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", birthdayCard);
+
+            }
+            else
+            {
+                return View(birthdayCard);
+            }
+        }
+    }
+}
---
+++ HomeWork6/Startup.cs
@@ -0,0 +1,48 @@
+﻿/******************************************
+ * 
+ * CSHP 320 A
+ * Assignment 6
+ * Colin Waldner
+ * June 7, 2021
+ * 
+ ****************************************/
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
